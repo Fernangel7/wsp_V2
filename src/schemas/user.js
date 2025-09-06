2f7d02cac1c9608b60ee889ea98f8b56b17ee790
#Fernangel <105268105+Fernangel7@users.noreply.github.com>
2025-09-05 23:07:06 -0700

    se añadio user.js
    Se añadio user.js en la ruta:
    
    src\schemas\user.js
    
    donde se configura el modelo en zod y se crean loa metodos para validar la informacion.

---
+++ src/schemas/user.js
@@ -0,0 +1,19 @@
+import z from 'zod'
+
+const zodUserSchema = z.object({
+  _id: z.uuid().nonoptional(),
+  name: z.string().nonoptional(),
+  mail: z.email().nonoptional(),
+  password: z.string().minLength(12).nonoptional(),
+  userID: z.number().int().nonoptional()
+})
+
+export async function partialUserValidation ({ data }) {
+  if (!z.json(data)) throw new Error('Invalid Data Format!')
+  return await zodUserSchema.partial().safeParseAsync(data)
+}
+
+export async function UserValidation ({ data }) {
+  if (!z.json(data)) throw new Error('Invalid Data Format!')
+  return await zodUserSchema.safeParseAsync(data)
+}
